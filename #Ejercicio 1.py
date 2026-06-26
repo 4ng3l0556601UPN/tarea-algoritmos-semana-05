fbff384b9efef3980a3eddb726d43b5e2a15dea8
wandarm <119546115+wandarm@users.noreply.github.com>
2026-06-26 11:31:58 -0500

    Add files via upload

---
+++ #Ejercicio 1.py
@@ -0,0 +1,6 @@
+#Uso def con la variable temporal nombre dentro de los paréntesis
+def saludar(nombre):
+    print(f" Hola {nombre} ,es hora de estudiar Python")
+
+#Pasa la cadena como argumento
+saludar("User")
