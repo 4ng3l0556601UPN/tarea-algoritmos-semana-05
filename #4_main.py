4d63222c7d3a4f89eb4d0a586deb39aef497d457
ashly-maker <aytoashly@gmail.com>
2026-06-26 21:08:31 -0500

    Add temperature conversion functionality

---
+++ #4_main.py
@@ -0,0 +1,7 @@
+from conversor import celsius_a_fahrenheit, fahrenheit_a_celsius
+
+c = 25
+f = 77
+
+print(f"{c} °C = {celsius_a_fahrenheit(c):.2f} °F")
+print(f"{f} °F = {fahrenheit_a_celsius(f):.2f} °C")
