362f43cd99fbf7ddde44750925b1009ca8e8b813
ashly-maker <aytoashly@gmail.com>
2026-06-26 21:24:40 -0500

    Add statistics functions to display list metrics

---
+++ 06_main.py
@@ -0,0 +1,8 @@
+from estadisticas import promedio, maximo, minimo
+
+numeros = [4, 8, 2, 10, 6]
+
+print("Lista:", numeros)
+print("Promedio:", promedio(numeros))
+print("Máximo:", maximo(numeros))
+print("Mínimo:", minimo(numeros))
